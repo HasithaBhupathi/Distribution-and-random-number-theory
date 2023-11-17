71057acaf28fd22a9e16a004304be7e9fd3a59ef
Hasitha Bhupathi <90141548+HasithaBhupathi@users.noreply.github.com>
2023-11-18 00:04:42 +0530

    Update linear_congruential_genarators.py

--- linear_congruential_genarators.py
+++ linear_congruential_genarators.py
@@ -1,24 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.zeros(1000,dtype='float')
+x = np.zeros(1001,dtype='float')
 u = np.zeros(1000,dtype='float')
 t = np.zeros(1000)
 
 a = 7**5
 c = 0
 m = (2**31)-1
+N = 1000
 
 x[0] = 1
-u[0] = x[0]/m
 
-for i in range(1,1000):
-  x[i] = (a*x[i-1]+c)%m
-  u[i] = x[i]/m
+for i in range(0,N):
+  x[i+1] = (a*x[i]+c)%m
+  u[i] = x[i+1]/m
 
 print(u)
 
-for i in range(0,1000):
+for i in range(0,N):
   t[i] = i
 
 plt.plot(t,u,'o')
