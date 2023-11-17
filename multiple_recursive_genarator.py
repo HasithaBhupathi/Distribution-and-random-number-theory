f4b08d93daf1c4147515fb4664711e18aa8e91a5
Hasitha Bhupathi <90141548+HasithaBhupathi@users.noreply.github.com>
2023-11-18 00:15:48 +0530

    Update multiple_recursive_genarator.py

--- multiple_recursive_genarator.py
+++ multiple_recursive_genarator.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.zeros(1000,dtype='float')
+x = np.zeros(1001,dtype='float')
 u = np.zeros(1000,dtype='float')
 t = np.zeros(1000)
 a = 7**5
@@ -11,20 +11,20 @@ m = (2**31)-1
 k = 5
 b = 1
 c = 0
+N = 1000
 
 x[0] = 1
-u[0] = x[0]/m
 
 for i in range(1,k):
   x[i] = (b*x[i-1]+c)%m
-  u[i] = x[i]/m
+  u[i-1] = x[i]/m
 
-for i in range(k,1000):
+for i in range(k,N):
   s = 0
   for r in range(1,k+1):
     s = s+a*x[i-r]
   x[i] = s%m
-  u[i] = x[i]/m
+  u[i-1] = x[i]/m
 
 for i in range(0,1000):
   t[i] = i
